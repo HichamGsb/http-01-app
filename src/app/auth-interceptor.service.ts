214b9a420396a88001d5d92ac43cae25e717a3b8
Hicham Guessab <hicham.guessab@cbainfo.fr>
2023-10-03 16:49:42 +0200

    Correction : remove Injectable

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { tap } from "rxjs/operators";
 
-@Injectable()
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // if (req.url) ...
