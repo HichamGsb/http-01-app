fe32336b0ec9229035010d12527594f10691d7a9
Hicham Guessab <hicham.guessab@cbainfo.fr>
2023-10-03 16:45:39 +0200

    Manipulating Request Objects

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -6,6 +6,10 @@ export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // if (req.url) ...
     console.log('Request is on its way');
-    return next.handle(req);
+    console.log(req.url);
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'zeaz')
+    });
+    return next.handle(modifiedRequest);
   }
 }
