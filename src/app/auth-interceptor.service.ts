293d17ea3ac70a1d3c9e7aa3986ed0e16685cb44
Hicham Guessab <hicham.guessab@cbainfo.fr>
2023-10-03 16:58:02 +0200

    Multiple Interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,22 +1,10 @@
-import { Injectable } from "@angular/core";
-import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { tap } from "rxjs/operators";
+import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    // if (req.url) ...
-    console.log('Request is on its way');
-    console.log(req.url);
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'zeaz')
     });
-    return next.handle(modifiedRequest)
-      .pipe(tap(event => {
-        console.log(event);
-        if (event.type === HttpEventType.Response) {
-          console.log('Response arrived, body data: ');
-          console.log(event.body);
-        }
-      }));
+    return next.handle(modifiedRequest);
   }
 }
