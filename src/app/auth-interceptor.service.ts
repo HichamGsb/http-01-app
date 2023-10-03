c12ce09270f3cafd1c1c2d3f0ab902fe4f2d653c
Hicham Guessab <hicham.guessab@cbainfo.fr>
2023-10-03 16:48:09 +0200

    Response Interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs/operators";
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor{
@@ -10,6 +11,13 @@ export class AuthInterceptorService implements HttpInterceptor{
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'zeaz')
     });
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest)
+      .pipe(tap(event => {
+        console.log(event);
+        if (event.type === HttpEventType.Response) {
+          console.log('Response arrived, body data: ');
+          console.log(event.body);
+        }
+      }));
   }
 }
