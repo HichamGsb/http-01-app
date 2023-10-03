494136b22de4b693689d1732c8b15d93dbefdd56
Hicham Guessab <hicham.guessab@cbainfo.fr>
2023-10-03 16:42:57 +0200

    Introducing Interceptors

---
+++ src/app/auth-interceptor.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor{
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    // if (req.url) ...
+    console.log('Request is on its way');
+    return next.handle(req);
+  }
+}
